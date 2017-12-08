d611d19abe17346b6d11d3983ae96a00fe2fb697
Unknown <baangaritar@unal.edu.co>
2017-12-08 16:00:02 -0500

    InternationalVet Creation

---
+++ InternationalVet/header.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>INTERNATIONAL VET</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/header.css">
+		<link rel="stylesheet" href="css/carousel.css">
+	</head>
+	<body>
+		<!-- header -->
+		<header id="header" class="">
+			<div class="row">
+				<div class="col-sm-5">
+					<div class="row">
+						<div class="col-sm-5 offset-sm-1">
+							<a href=""><img src="images/logo.png" alt="logo-international-vet.png" height="160" ></a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-7 hidden-md-down">
+					<div class="row ">
+						<div class="col-sm-5">
+							<a href="">
+								<img src="images/logo-skyla.png" alt="logo-distributor-vet.png" height="80" class="sponsor-logo hidden-sm-down" >
+								<p class="logo-caption">Distribuidor exclusivo para Colombia</p>
+							</a>
+						</div>
+						<div class="col-sm-5">
+							<div class="row social-icons float-right">
+								<div class="col-sm-12 offset-6 ">
+									<ul class="list-inline">
+										<li class="list-inline-item">
+											<a href="" class="fa fa-facebook-square"></a>
+										</li>
+										<li class="list-inline-item">
+											<a href="" class="fa fa-instagram"></a>
+										</li>
+										<li class="list-inline-item">
+											<a href="" class="fa fa-youtube-play"></a>
+										</li>
+									</ul>
+								</div>
+								
+							</div>
+						</div>
+					</div>
+					<form action="index_submit" method="get" accept-charset="utf-8">
+						<input type="search" name="search" placeholder="¿Qué estás buscando?" class="search">
+						<button type="search" class="btn btn-secondary btn-search fa fa-search"></button>
+					</form>
+				</div>
+			</div>
+		</header>
+		<!-- /header -->
+		<!-- navbar -->
+		<nav class="navbar navbar-expand-sm navbar-light bg-info">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls=".navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">QUIENES SOMOS <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">EQUIPOS</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">EVENTOS Y CAPACITACIONES</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">DESCARGAS</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">CONTACTO</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<!-- /navbar -->
---
+++ InternationalVet/index.php
@@ -0,0 +1,30 @@
+		
+<?php include 'header.php'; ?>
+		<section>
+			<div id="carousel-slide" class="carousel slide" data-ride="carousel">
+				<div class="carousel-inner" role="listbox">
+					<div class="carousel-item active">
+						<img src="images/image1.jpg" alt="Primer slide">
+						<div class="carousel-caption d-none d-md-block">
+							<h3>...</h3>
+							<p>...</p>
+						</div>
+					</div>
+					<div class="carousel-item">
+						<img class="d-block img-fluid" src="" alt="Second slide">
+					</div>
+					<div class="carousel-item">
+						<img class="d-block img-fluid" src="" alt="Third slide">
+					</div>
+				</div>
+				<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+				</a>
+			</div>
+		</section>
+<?php include 'footer.php'; ?>
